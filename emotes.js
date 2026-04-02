b50c33cd1642055c7ed9db3082296b1c769b9d76
cj1063 <charliejanes27@outlook.com>
2026-04-02 18:50:31 +0000

    modified:   emotes.js

--- emotes.js
+++ emotes.js
@@ -1,8 +1,8 @@
 const items = [":3", "OwO", ">:3"];
 const el = document.getElementById("emotes");
-let i = 0; // Start at 0 to catch the first item
+let i = 0;
 
 setInterval(() => {
   el.textContent = items[i % items.length];
-  i++; // Increment i so the next cycle uses the next index
+  i++;
 }, 250);
